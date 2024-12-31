a681b2992667818d320b0e09c7025409e2d92ba0
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2024-12-31 11:46:17 +0300

    all lib split

--- NF/UtilitesString.NF/Properties/AssemblyInfo.cs
+++ NF/UtilitesString.NF/Properties/AssemblyInfo.cs
@@ -5,24 +5,24 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Utility")]
-[assembly: AssemblyDescription("Вспомогательные классы")]
+[assembly: AssemblyTitle("UtilitesString")]
+[assembly: AssemblyDescription("Вспомогательные классы работа со строками")]
 [assembly: AssemblyConfiguration("")]
 // [assembly: AssemblyCompany("")]
 [assembly: AssemblyCompany("doctorRaz@gmail.com")]
-[assembly: AssemblyProduct("Utility.NF")]
+[assembly: AssemblyProduct("UtilitesString.NF")]
 // [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyCopyright("© Разыграев Андрей 2014-2025")]
 [assembly: AssemblyTrademark("©doctorRAZ")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyInformationalVersion("Utility")]
+[assembly: AssemblyInformationalVersion("UtilitesString")]
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1BEA14B2-A7C5-9E9A-A926-FBC1564E3DE9")]
+[assembly: Guid("1923BD11-0E59-88CB-5FF6-ABE58AB4C5BC")]
 
 // Version information for an assembly consists of the following four values:
 //
